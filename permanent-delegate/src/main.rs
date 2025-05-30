614e20ac171423b18b93545bfa0416f202ec87ce
ethsdev <tensortrust@gmail.com>
2025-05-30 20:49:52 +0700

    update: add mint initialization and token transfer logic

--- permanent-delegate/src/main.rs
+++ permanent-delegate/src/main.rs
@@ -1,3 +1,5 @@
+mod utils;
+
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Keypair, signer::Signer,
@@ -13,202 +15,137 @@ use spl_token_2022::{
     },
     state::Mint,
 };
+use utils::check_request_airdrop;
 
 fn main() {
-    let mint_authority = Keypair::new();
-    let mint_account = Keypair::new();
-
-    let decimals = 0;
-
-    println!("AUTHORITY: {}", mint_authority.pubkey());
-    println!("MINT: {}", mint_account.pubkey());
-    println!("DELEGATE: {}", mint_authority.pubkey());
-    println!("TOKEN DECIMALS: {}", decimals);
-
     let client = RpcClient::new("http://localhost:8899".to_string());
 
-    check_request_airdrop(&client, &mint_authority.pubkey(), 2);
-
-    let extensions = [ExtensionType::PermanentDelegate];
-    let mint_len = ExtensionType::try_calculate_account_len::<Mint>(&extensions).unwrap();
-    let rent = client
-        .get_minimum_balance_for_rent_exemption(mint_len)
-        .unwrap();
-
-    let create_account_instr = system_instruction::create_account(
-        &mint_authority.pubkey(),
-        &mint_account.pubkey(),
-        rent,
-        mint_len as u64,
-        &spl_token_2022::id(),
-    );
-
-    let init_permanent_delegate_instr = initialize_permanent_delegate(
-        &spl_token_2022::id(),
-        &mint_account.pubkey(),
-        &mint_authority.pubkey(),
-    )
-    .unwrap();
-
-    let init_mint_instr = initialize_mint(
-        &spl_token_2022::id(),
-        &mint_account.pubkey(),
-        &mint_authority.pubkey(),
-        Some(&mint_authority.pubkey()),
-        decimals,
-    )
-    .unwrap();
-
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[
-            create_account_instr,
-            init_permanent_delegate_instr,
-            init_mint_instr,
-        ],
-        Some(&mint_authority.pubkey()),
-        &[&mint_authority, &mint_account],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
-
+    let mint_authority = Keypair::new();
+    let mint_account = Keypair::new();
     let party1 = Keypair::new();
-    let party1_ata = get_associated_token_address_with_program_id(
-        &party1.pubkey(),
-        &mint_account.pubkey(),
-        &spl_token_2022::id(),
-    );
 
-    let mint_authority_ata = get_associated_token_address_with_program_id(
-        &mint_authority.pubkey(),
-        &mint_account.pubkey(),
-        &spl_token_2022::id(),
-    );
-    let mint_authority_ata_instr = create_associated_token_account(
-        &mint_authority.pubkey(),
-        &mint_authority.pubkey(),
-        &mint_account.pubkey(),
-        &spl_token_2022::id(),
-    );
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[mint_authority_ata_instr],
-        Some(&mint_authority.pubkey()),
-        &[&mint_authority],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
+    let decimals = 0;
+    let mint_pubkey = mint_account.pubkey();
+
+    println!("AUTHORITY: {}", mint_authority.pubkey());
+    println!("MINT: {}", mint_pubkey);
+    println!("TOKEN DECIMALS: {}", decimals);
+
+    // Ensure accounts are funded
+    check_request_airdrop(&client, &mint_authority.pubkey(), 2);
+    check_request_airdrop(&client, &party1.pubkey(), 2);
+
+    initialize_token_mint(&client, &mint_authority, &mint_account, decimals);
+
+    let mint_authority_ata = create_ata(&client, &mint_authority, &mint_authority.pubkey(), &mint_pubkey);
+    let party1_ata = create_ata(&client, &party1, &party1.pubkey(), &mint_pubkey);
 
     println!("PARTY1: {}", party1.pubkey());
     println!("PARTY1_ATA: {}", party1_ata);
     println!("MINT_AUTHORITY_ATA: {}", mint_authority_ata);
 
-    let party1_ata_instr = create_associated_token_account(
-        &party1.pubkey(),
-        &party1.pubkey(),
-        &mint_account.pubkey(),
-        &spl_token_2022::id(),
-    );
-
-    check_request_airdrop(&client, &party1.pubkey(), 2);
-
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[party1_ata_instr],
-        Some(&party1.pubkey()),
-        &[&party1],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
-
-    let mint_to_instr = mint_to(
-        &spl_token_2022::id(),
-        &mint_account.pubkey(),
-        &party1_ata,
-        &mint_authority.pubkey(),
-        &[&mint_authority.pubkey(), &mint_account.pubkey()],
-        10,
-    )
-    .unwrap();
-
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[mint_to_instr],
-        Some(&mint_authority.pubkey()),
-        &[&mint_authority, &mint_account],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
-
-    let delegate_transfer_to_instr = transfer_checked(
-        &spl_token_2022::id(),
-        &party1_ata,
-        &mint_account.pubkey(),
-        &mint_authority_ata,
-        &mint_authority.pubkey(),
-        &[&mint_authority.pubkey()],
-        10,
-        0,
-    )
-    .unwrap();
-
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[delegate_transfer_to_instr],
-        Some(&mint_authority.pubkey()),
-        &[&mint_authority],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
-
-    let burn_instr = burn_checked(
-        &spl_token_2022::id(),
-        &mint_authority_ata,
-        &mint_account.pubkey(),
-        &mint_authority.pubkey(),
-        &[&mint_authority.pubkey()],
-        3,
-        0,
-    )
-    .unwrap();
-
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    let tx = Transaction::new_signed_with_payer(
-        &[burn_instr],
-        Some(&mint_authority.pubkey()),
-        &[&mint_authority],
-        recent_blockhash,
-    );
-    client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .unwrap();
+    mint_tokens(&client, &mint_authority, &mint_account, &party1_ata, 10);
+    transfer_tokens(&client, &mint_authority, &mint_pubkey, &party1_ata, &mint_authority_ata, 10);
+    burn_tokens(&client, &mint_authority, &mint_pubkey, &mint_authority_ata, 3);
 }
 
-fn check_request_airdrop(client: &RpcClient, account: &Pubkey, amount: u64) {
-    if client.get_balance(&account).unwrap().eq(&0) {
-        client
-            .request_airdrop(&account, LAMPORTS_PER_SOL * amount)
-            .unwrap();
+fn initialize_token_mint(client: &RpcClient, authority: &Keypair, mint: &Keypair, decimals: u8) {
+    let extensions = [ExtensionType::PermanentDelegate];
+    let mint_len = ExtensionType::try_calculate_account_len::<Mint>(&extensions).unwrap();
+    let rent = client.get_minimum_balance_for_rent_exemption(mint_len).unwrap();
 
-        loop {
-            if (LAMPORTS_PER_SOL).gt(&client.get_balance(&account).unwrap()) {
-                println!("Airdrop for {} has not reflected ...", account);
-                std::thread::sleep(std::time::Duration::from_secs(1));
-            } else {
-                println!("\nAirdrop for {} has reflected!\n", account);
+    let tx = Transaction::new_signed_with_payer(
+        &[
+            system_instruction::create_account(
+                &authority.pubkey(),
+                &mint.pubkey(),
+                rent,
+                mint_len as u64,
+                &spl_token_2022::id(),
+            ),
+            initialize_permanent_delegate(&spl_token_2022::id(), &mint.pubkey(), &authority.pubkey()).unwrap(),
+            initialize_mint(
+                &spl_token_2022::id(),
+                &mint.pubkey(),
+                &authority.pubkey(),
+                Some(&authority.pubkey()),
+                decimals,
+            ).unwrap(),
+        ],
+        Some(&authority.pubkey()),
+        &[authority, mint],
+        client.get_latest_blockhash().unwrap(),
+    );
+    client.send_and_confirm_transaction_with_spinner(&tx).unwrap();
+}
 
-                break;
-            }
-        }
-    }
+fn create_ata(client: &RpcClient, payer: &Keypair, owner: &Pubkey, mint: &Pubkey) -> Pubkey {
+    let ata = get_associated_token_address_with_program_id(owner, mint, &spl_token_2022::id());
+    let tx = Transaction::new_signed_with_payer(
+        &[create_associated_token_account(
+            &payer.pubkey(),
+            owner,
+            mint,
+            &spl_token_2022::id(),
+        )],
+        Some(&payer.pubkey()),
+        &[payer],
+        client.get_latest_blockhash().unwrap(),
+    );
+    client.send_and_confirm_transaction_with_spinner(&tx).unwrap();
+    ata
+}
+
+fn mint_tokens(client: &RpcClient, authority: &Keypair, mint: &Keypair, destination: &Pubkey, amount: u64) {
+    let tx = Transaction::new_signed_with_payer(
+        &[mint_to(
+            &spl_token_2022::id(),
+            &mint.pubkey(),
+            destination,
+            &authority.pubkey(),
+            &[&authority.pubkey(), &mint.pubkey()],
+            amount,
+        ).unwrap()],
+        Some(&authority.pubkey()),
+        &[authority, mint],
+        client.get_latest_blockhash().unwrap(),
+    );
+    client.send_and_confirm_transaction_with_spinner(&tx).unwrap();
+}
+
+fn transfer_tokens(client: &RpcClient, authority: &Keypair, mint: &Pubkey, source: &Pubkey, destination: &Pubkey, amount: u64) {
+    let tx = Transaction::new_signed_with_payer(
+        &[transfer_checked(
+            &spl_token_2022::id(),
+            source,
+            mint,
+            destination,
+            &authority.pubkey(),
+            &[&authority.pubkey()],
+            amount,
+            0,
+        ).unwrap()],
+        Some(&authority.pubkey()),
+        &[authority],
+        client.get_latest_blockhash().unwrap(),
+    );
+    client.send_and_confirm_transaction_with_spinner(&tx).unwrap();
+}
+
+fn burn_tokens(client: &RpcClient, authority: &Keypair, mint: &Pubkey, from: &Pubkey, amount: u64) {
+    let tx = Transaction::new_signed_with_payer(
+        &[burn_checked(
+            &spl_token_2022::id(),
+            from,
+            mint,
+            &authority.pubkey(),
+            &[&authority.pubkey()],
+            amount,
+            0,
+        ).unwrap()],
+        Some(&authority.pubkey()),
+        &[authority],
+        client.get_latest_blockhash().unwrap(),
+    );
+    client.send_and_confirm_transaction_with_spinner(&tx).unwrap();
 }
